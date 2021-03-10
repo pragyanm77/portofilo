9f526b28dc9b938c86d6dd7b461d1ca1f40daaa6
pragyanm77 <78930539+pragyanm77@users.noreply.github.com>
2021-03-09 23:32:24 -0500

    buttons

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,48 @@ function myFunction() {
     element.classList.toggle("light-mode");
 
 }
+
+//Button for Function 1//
+function function1() {
+    alert('Something very cool coming soon (Function A)')
+    } 
+  
+//Button for Function 2//
+  function function2() {
+    alert('Something very cool coming soon (Function B)')
+    } 
+
+//Button for Function 3//
+  function function3() {
+    alert('Something very cool coming soon (Function C)')
+    } 
+  
+//Button for Function 4//
+  function function4() {
+    alert('Something very cool coming soon (Function D)')
+    } 
+  
+//Button for Function 5//
+  function function5() {
+    alert('Something very cool coming soon (Function E)')
+    } 
+
+//Button for Function 6//  
+  function function6() {
+    alert('Something very cool coming soon (Function F)')
+    } 
+  
+//Button for Function 7//
+  function function7() {
+    alert('Something very cool coming soon (Function G)')
+    } 
+
+//Button for Function 8//
+  function function8() {
+    alert('Something very cool coming soon (Function H)')
+    } 
+
+//Button for Function 9//
+  function function9() {
+    alert('Something very cool coming soon (Function I)')
+    } 
