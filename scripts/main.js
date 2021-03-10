1ed6c53d7ae54191bc42251cfd22f4fc14fc3195
pragyanm77 <78930539+pragyanm77@users.noreply.github.com>
2021-03-10 13:36:23 -0500

    edited buttons

--- scripts/main.js
+++ scripts/main.js
@@ -8,45 +8,45 @@ function myFunction() {
 
 //Button for Function 1//
 function function1() {
-    alert('Something very cool coming soon (Function A)')
+    alert('Something very cool coming soon (Assignment A)')
     } 
   
 //Button for Function 2//
   function function2() {
-    alert('Something very cool coming soon (Function B)')
+    alert('Something very cool coming soon (Assignment B)')
     } 
 
 //Button for Function 3//
   function function3() {
-    alert('Something very cool coming soon (Function C)')
+    alert('Something very cool coming soon (Assignment C)')
     } 
   
 //Button for Function 4//
   function function4() {
-    alert('Something very cool coming soon (Function D)')
+    alert('Something very cool coming soon (Assignment D)')
     } 
   
 //Button for Function 5//
   function function5() {
-    alert('Something very cool coming soon (Function E)')
+    alert('Something very cool coming soon (Assignment E)')
     } 
 
 //Button for Function 6//  
   function function6() {
-    alert('Something very cool coming soon (Function F)')
+    alert('Something very cool coming soon (Assignment F)')
     } 
   
 //Button for Function 7//
   function function7() {
-    alert('Something very cool coming soon (Function G)')
+    alert('Something very cool coming soon (Assignment G)')
     } 
 
 //Button for Function 8//
   function function8() {
-    alert('Something very cool coming soon (Function H)')
+    alert('Something very cool coming soon (Assignment H)')
     } 
 
 //Button for Function 9//
   function function9() {
-    alert('Something very cool coming soon (Function I)')
+    alert('Something very cool coming soon (Assignment I)')
     } 
