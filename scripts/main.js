e42ef6db62fdb4b92611d8352fd85f97f71aec5e
pragyanm77 <78930539+pragyanm77@users.noreply.github.com>
2021-03-23 09:53:11 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ function largernumber() {
 }
 
 //Button for Function 2 (sign of product)//
-//enter 3 numbers, progarm nultiplies the numbers and gives sign of the product//
+//enter 3 numbers, progarm multiplies the numbers and gives sign of the product (why testing is it is elow or above 0)//
   function signOfProduct() {
     let signnum1 = parseInt(prompt("Insert a NUMBER"));
     let signnum2 = parseInt(prompt("Insert a another NUMBER"));
@@ -47,20 +47,20 @@ function largernumber() {
   } 
 
 //Button for Function 3 (number sort)//
-//enter 3 numbers and sort from low to high. each number is assigned a varible//
+//enter 3 numbers and sort from low to high. each number is assigned a varible, then one of the 6 possiblities is outputed//
   function threeNumberSort() {
     let sortnum1 = parseInt(prompt("Insert a NUMBER"));
     let sortnum2 = parseInt(prompt("Insert a another NUMBER"));
     let sortnum3 = parseInt(prompt("Insert one more NUMBER"));
     
 
-    if (sortnum1<=sortnum2&&sortnum2<=sortnum3){
+    if (sortnum1 <= sortnum2 && sortnum2 <= sortnum3){
       document.getElementById("button3").innerHTML = sortnum1+ "<" + sortnum2 + "<" + sortnum3;
     }
-    else if (sortnum1<=sortnum3&&sortnum3<=sortnum2){
+    else if (sortnum1 <= sortnum3 && sortnum3 <= sortnum2){
       document.getElementById("button3").innerHTML = sortnum1+ "<" + sortnum3 + "<" + sortnum2;
     }
-    else if (sortnum2<=sortnum1&&sortnum1<=sortnum3){
+    else if (sortnum2 <= sortnum1 && sortnum1 <= sortnum3){
       document.getElementById("button3").innerHTML = sortnum2+ "<" + sortnum1 + "<" + sortnum3;
     }
     else if (sortnum2<=sortnum3&&sortnum3<=sortnum1){
@@ -79,36 +79,38 @@ function largernumber() {
 
   
 //Button for Function 4 (forSumThrees)//
-//using for loops setting conditions for and the loop. and preform only if i is a multiple of 3 with 0 remainder//
+//using for loops setting conditions for and the loop. and preform only if i is a multiple of 3 with 0 remainder. The output is the sum of all multiples of 3's up to 1000 //
   function forSumThrees() {
-    let forloop= 0
+    let runningTotal= 0;
+    let sumOfThrees=0;
     for (let i=1; i<=1000; i++){
       if(i % 3 === 0){
-        document.getElementById("button4").innerHTML = forloop +=i
+        sumOfThrees = runningTotal +=i;
       }
     }
+    document.getElementById("button4").innerHTML= sumOfThrees;
   }
 
   
 //Button for Function 5 (whileSumThrees)//
-////using while loops setting conditions for and the loop. and preform only if i is a multiple of 3 with 0 remainder//
+////using while loops setting conditions for and the loop. and preform only if i is a multiple of 3 with 0 remainder. The output is the sum of all multiples of 3's up to 1000//
   function whileSumThrees() {
-  let whileloop= 0;
+  let runningtotal= 0;
   let i = 0;
     while(i<1000){
       i++
       if(i%3===0){
-        document.getElementById("button5").innerHTML = whileloop += i
+        document.getElementById("button5").innerHTML = runningtotal += i;
       }
     }  
   }
 
 //Button for Function 6 (tenFour)//  
-//finding the multiples if 4,10 and both in console using forloops//
+//finding the multiples if 4,10 and both in console using forloops. outputs onto console.log//
   function tenFour() {
     let forloop = 0
     for (let i=1; i<=100; i++){
-      if (i%4===0&&i%10===0){
+      if (i%4===0 && i%10===0){
         console.log ("TenFour!");
       }
       else if (i%10===0){
@@ -125,11 +127,12 @@ function largernumber() {
 
   
 //Button for Function 7 (centuryFromYear)//
-//rounding up the the nearest 100 to find the century the input year is in//
+//rounding up the the nearest 100 to find the century the input year is in by using math.ceil//
   function  centuryFromYear() {
     let yearnum1 = parseInt(prompt("Enter the year please: "))
     if (yearnum1>0){
-    document.getElementById("button7").innerHTML= "This year is in the " +  Math.ceil(yearnum1/100)+"th"+ " century"
+      // math.ceil rounds the number up to the nearest 100 //
+      document.getElementById("button7").innerHTML= "This year is in the " +  Math.ceil (yearnum1/100) +"th"+ " century"
     }
     else{
       document.getElementById("button7").innerHTML= "Make sure you have entered a real number "
